ff679a02d3b0c379e90ac01c8568a8a1ce66fa73
Maria Thompson <130598863+silverjie1@users.noreply.github.com>
2025-04-15 11:24:39 +0800

    Create awsjp.sh

---
+++ awsjp.sh
@@ -0,0 +1,4 @@
+S=v4 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t b7139130-8f9d-4eca-b17c-7f7e382b7ad7 -u https://ny.188002.xyz"
+S=v6 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 81dd0034-434b-4270-83a4-3083d75f157f -u https://ny.188002.xyz"
+S=BANANALINK bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t f339e7ee-6f03-4aff-92aa-202f442ed0eb -u https://37838527.bananalink.one"
+bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl --system
