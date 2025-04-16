40643dc61ef2240cc5e0ffc6ace6994ecd99abfd
Maria Thompson <130598863+silverjie1@users.noreply.github.com>
2025-04-17 01:20:16 +0800

    Update GCPHK.sh

--- GCPHK.sh
+++ GCPHK.sh
@@ -1,5 +1,4 @@
 S=v4 OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t a95ad1f2-5d58-48b2-8dbf-c628f59dbca3 -u https://ny.188002.xyz"
 S=v6 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ccc1f732-b6da-4cb8-9052-5b986b15497e -u https://ny.188002.xyz"
 S=BANANALINK bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 4dd0bf8c-ab09-4994-ac26-911dbf9cdadc -u https://37838527.bananalink.one"
-S=cy1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ba141581-220e-430d-bce7-c17b8d96800b -u https://ny.congyu.org"
 bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl --system
