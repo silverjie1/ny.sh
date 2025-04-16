70e647f80a93ded20db14b9391e36bfe66ae0b0a
Maria Thompson <130598863+silverjie1@users.noreply.github.com>
2025-04-16 16:33:14 +0800

    Update and rename AWSHK.sh to awshk.sh

--- awshk.sh
+++ awshk.sh
@@ -1,3 +1,3 @@
-S=v4 OPTIMIZE=1 Ibash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3f1c5b02-3377-4b05-b72b-6678ba41d47a -u https://ny.188002.xyz"
+S=v4 OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3f1c5b02-3377-4b05-b72b-6678ba41d47a -u https://ny.188002.xyz"
 S=v4 Ibash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t e9b22e2b-9461-4602-9a1d-26246fd7e895 -u https://ny.188002.xyz"
 bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl --system
