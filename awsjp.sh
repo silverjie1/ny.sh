712f282717b6fe0d7336c139bd839138718c2064
Maria Thompson <130598863+silverjie1@users.noreply.github.com>
2025-04-25 14:42:21 +0800

    Update awsjp.sh

--- awsjp.sh
+++ awsjp.sh
@@ -1,4 +1,5 @@
 S=v4 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t b7139130-8f9d-4eca-b17c-7f7e382b7ad7 -u https://ny.188002.xyz"
 S=v6 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 81dd0034-434b-4270-83a4-3083d75f157f -u https://ny.188002.xyz"
 S=BANANALINK bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t f339e7ee-6f03-4aff-92aa-202f442ed0eb -u https://37838527.bananalink.one"
+S=null bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 1fa0194c-48e6-4970-87fd-6e0ee6bc345c -u https://ny.papawall.cc"
 bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl --system
