20d5e719b553639b36718e55afe6b1636e06e8c2
Maria Thompson <130598863+silverjie1@users.noreply.github.com>
2025-04-25 14:44:42 +0800

    Update awshk.sh

--- awshk.sh
+++ awshk.sh
@@ -1,4 +1,4 @@
 S=awsv4 OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3f1c5b02-3377-4b05-b72b-6678ba41d47a -u https://ny.188002.xyz"
 S=awsv6 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t e9b22e2b-9461-4602-9a1d-26246fd7e895 -u https://ny.188002.xyz"
-S=null bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 32a5b89d-be0f-40c9-9704-8413ce849bbd -u https://ny.papawall.cc"
+S=null bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t a43eba7c-493a-4e3f-a7b5-5982a0bebce0 -u https://ny.papawall.cc"
 bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl --system
