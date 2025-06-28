cef467bc990719d222fe8897ca772ec2c8d21947
Maria Thompson <130598863+silverjie1@users.noreply.github.com>
2025-06-28 23:31:56 +0800

    Update awshk.sh

--- awshk.sh
+++ awshk.sh
@@ -1,5 +1,6 @@
 S=awsv4 OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3f1c5b02-3377-4b05-b72b-6678ba41d47a -u https://ny.188002.xyz"
 S=awsv6 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t e9b22e2b-9461-4602-9a1d-26246fd7e895 -u https://ny.188002.xyz"
+S=BANANALINK bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 4dd0bf8c-ab09-4994-ac26-911dbf9cdadc -u https://37838527.bananalink.one"
 S=null bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t a43eba7c-493a-4e3f-a7b5-5982a0bebce0 -u https://ny.papawall.cc"
 S=zhuji bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 570250d8-66e5-4820-899a-668b809af8f0 -u https://bbq.zhujipai.cloud"
 S=nygou bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t f7e0808f-001f-46cb-9c94-62cc30e4622a -u https://ny.gou.wtf"
